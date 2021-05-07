88756a3d446e5983a5e8702bc480928837639c50
NENUKORAH <81701640+NENUKORAH@users.noreply.github.com>
2021-05-06 21:28:34 -0400

    Adding the election audit code.

--- python_practice.py
+++ python_practice.py
@@ -55,7 +55,3 @@ message_to_candidate = (
     f"You received {candidate_votes} number of votes. "
     f"The total number of votes in the election was {total_votes}. "
     f"You received {candidate_votes / total_votes * 100}% of the total votes.")
-
-print(message_to_candidate)
-
-print(f'{my_dict}{Nnaemeka}')
